aa35f47465fe199fa35cfabe6ac941b2cd39007d
Ed Millington <e.r.millington@dunelm.org.uk>
2016-09-07 09:11:21 +1200

    Added a second guard
    And the ability to add many guards in map1.js!

--- map1.js
+++ map1.js
@@ -11,3 +11,11 @@ const map1 = {
         '################',
     ]
 }
+
+let player = {x:1, y:1, char:'@', currentTile:'.'}
+
+let guard0 = {x:3, y:4, char:'G', currentTile:'.'}
+
+let guard1 = {x:7, y:1, char:'G', currentTile:'.'}
+
+let guards = [guard0, guard1]
--- rouge.js
+++ rouge.js
@@ -2,10 +2,6 @@ let maze = map1.maze
 
 let map = maze.map(line => line.split(''))
 
-let player = {x:1, y:1, char:'@', currentTile:'.'}
-
-let guard = {x:3, y:4, char:'G', currentTile:'.'}
-
 const keyCodeToDirection = {
     37: {x: -1, y: 0},
     38: {x: 0, y: -1},
@@ -31,26 +27,44 @@ function canHear(entity1, entity2) {
     return rectAroundPoint(player, 4).some(point => point.x === entity2.x && point.y === entity2.y)
 }
 
-function scan(entity1, entity2) {
-    //make sure we can see the entity unless it needs to be hidden
-    showEntity(entity2)
+function scan(entity1, entity2) {   
     //can entity1 see entity2?
     if (canSee(entity1, entity2)) {
-        //if so, great!
-        document.getElementById('text').innerHTML = 'You see a guard. He can see you an all'
+        return 1
     } else {
         //if not, check if entity1 can hear entity 2...
         if (canHear(entity1, entity2)) {
-            document.getElementById('text').innerHTML = 'You hear a guard behind the wall'
+            return 2
         } else {
-            if (entity1.char === '@') {
-                hideEntity(entity2)
-            }
+            return 3
         }   
     }
+}
 
-    if (entity1.char === '@' && entity1.currentTile === ',') {
-        document.getElementById('text2').innerHTML = 'Shuffle shuffle shuffle. Grass makes less noise'
+function playerScan(player, guards) {
+    var noOfGuardsScanned = 0;
+    for (i = 0; i < guards.length; i++) {
+        //make sure we can see the entity unless it needs to be hidden
+        showEntity(guards[i])
+        if (scan(player,guards[i]) === 3) {
+            hideEntity(guards[i])  
+        }
+        noOfGuardsScanned++
+    }
+    if (noOfGuardsScanned === guards.length) {
+        return true
+    }
+}
+
+function guardsMove(guards) {
+    var noOfGuardsMoved = 0;
+    for (i = 0; i < guards.length; i++) {
+        const guardDirection = keyCodeToDirection[randomDirection(37,40)]
+        guards[i] = move(guards[i], guardDirection) 
+        noOfGuardsMoved++
+    }
+    if (noOfGuardsMoved === guards.length) {
+        return true
     }
 }
 
@@ -62,22 +76,26 @@ function showEntity(entity) {
 }
     
 document.onkeydown = function(e) {
+    document.getElementById('text').innerHTML = ''
+    document.getElementById('text2').innerHTML = ''
+
     const direction = keyCodeToDirection[e.keyCode]
-    const guardDirection = keyCodeToDirection[randomDirection(37,40)]
+    
     if (direction) {
         //stop screen scroll
         e.preventDefault()
         player = move(player, direction)
-        guard = move(guard, guardDirection)
-        scan(player, guard)
-        render(map)
+        
+        if (guardsMove(guards)) {
+            if (playerScan(player, guards)) {
+                render(map)
+            }
+        }
     }
 }  
 
 function move(entity, direction) {
-    document.getElementById('text').innerHTML = ''
-    document.getElementById('text2').innerHTML = ''
-
+    
     const newPosition = addPoint(entity, direction)
     const newTile = map[newPosition.y][newPosition.x]
     if (newTile === '.') {
@@ -94,5 +112,13 @@ function teleportEntity(entity, {x, y}, newTile) {
     return{x, y, char: entity.char, currentTile: newTile}
 }
 
-//hideEntity(guard)
-render(map)
+
+if (playerScan(player, guards)) {
+    render(map)
+}
+
+
+
+//document.getElementById('text').innerHTML = 'You see a guard. He can see you an all'
+//document.getElementById('text').innerHTML = 'You hear a guard behind the wall'
+//document.getElementById('text2').innerHTML = 'Shuffle shuffle shuffle. Grass makes less noise'
